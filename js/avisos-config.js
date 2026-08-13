3d872725e711bd57903761930c297c3bf5035119
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2026-08-12 20:04:37 -0500

    Actualización: alertas.

--- js/avisos-config.js
+++ js/avisos-config.js
@@ -34,6 +34,15 @@ export const AVISOS = [
     mensaje: 'Las 72 horas son el tiempo más crítico (la "ventana de oro") después de un terremoto para rescatar a personas con vida bajo los escombros. Recuerda hacer silencio para que los rescatistas puedan escuchar a los sobrevivientes.',
     link: 'https://www.infobae.com/colombia/2026/08/12/por-que-son-tan-importantes-las-primeras-72-horas-tras-un-desastre-natural-que-es-la-ventana-de-oro/',
     textoLink: '🔗 Ver el detalle oficial'
+  },
+  {
+    id: 'ventana-terremoto',
+    titulo: '🚫🏠 Toque de queda.',
+    mensaje: 'La Alcaldía de Cali decretó un nuevo toque de queda para la noche de este miércoles 12 de agosto de 2026. La medida rige desde las 9:00 p. m. hasta las 6:00 a. m. del jueves 13 de agosto, como parte de las acciones por el terremoto y la calamidad pública.',
+    link: 'https://www.infobae.com/colombia/2026/08/12/por-que-son-tan-importantes-las-primeras-72-horas-tras-un-desastre-natural-que-es-la-ventana-de-oro/',
+    textoLink: '🔗 Ver el detalle oficial',
+    horaInicio: '21:00',
+    horaFin: '06:00',
   }
 ];
 
