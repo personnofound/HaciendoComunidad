0e96a7e528ca671c89beff00b2aab9b87f3e680c
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2026-08-16 21:53:34 -0500

    Actualización de las noticias.

--- js/avisos-config.js
+++ js/avisos-config.js
@@ -7,6 +7,13 @@ export const AVISOS = [
     mensaje: 'El ADRES reconocerá el pago de indemnizaciones y gastos funerarios para las víctimas mortales de catástrofes y/o eventos naturales. Así como el pago de servicios de salud prestados a las víctimas de estos eventos.',
     link: 'https://share.google/dZMg7HqRKzdlSiVjk',
     textoLink: '🔗 Ver el detalle oficial'
+  },
+  {
+    id: 'entrada-cali-km18',
+    titulo: '🚗 Gestión de movilidad: Restricción de vehículos que ingresan por el KM-18.',
+    mensaje: 'Lunes, 17 de agosto, habrá restricciones para vehículos que ingresan a Cali por el KM-18. Placas pares pueden ingresar de 4:00 p. m. a 6:00 p. m. Placas impares pueden ingresar de 6:00 p. m. a 8:00 p. m. La medida no aplica para los carros que salen de la ciudad.',
+    link: 'https://www.instagram.com/movilidadcali/p/DcHS9G0Gdhx/?hl=es',
+    textoLink: '🔗 Ver el detalle oficial'
   }
 ];
 
